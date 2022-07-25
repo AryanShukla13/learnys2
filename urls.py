ff013e6c9e6d40827b69884b7e136713c9deabd7
AryanShukla13 <101211852+AryanShukla13@users.noreply.github.com>
2022-07-26 01:47:38 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,13 +1,41 @@
+"""core URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
-from django.urls import path
-from article.views import article
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 from home import views
+from django.views.static import serve
+# from django.conf.urls import re_path as url
+from django.urls import include, path as url
+# from myapp.views import home
+
+
+admin.site.site_header = "Learnys Admin"
+admin.site.site_title = "Learnys Admin Portal"
+admin.site.index_title = "Welcome to Learnys Portal"
+
 
 urlpatterns = [
-    path("",views.index,name='home'),
-    path("about",views.about,name='about'),
-    path("services",views.services,name='services'),
-    path("contact",views.contact,name='contact'),
-    path("article",article,name='article')
-    
+    path('admin/', admin.site.urls),
+    path('',include('home.urls')),
+    path('signup',views.handleSignup,name='handleSignup'),url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
